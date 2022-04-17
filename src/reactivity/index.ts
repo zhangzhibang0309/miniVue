2d4a439ebcc52a0e02b90b54f8e217c6076c8073
zhangzhibang0309 <295854713@qq.com>
2022-04-17 19:57:03 +0800

    reactiv-effect-依赖收集-依赖触发

--- src/reactivity/index.ts
+++ src/reactivity/index.ts
@@ -1,5 +1,3 @@
-
-
 export function add(a, b) {
   return a + b
 }
--- src/reactivity/tests/effect.spec.ts
+++ src/reactivity/tests/effect.spec.ts
@@ -1,3 +1,6 @@
+import { effect } from "../effect"
+import { reactive } from "../reactive"
+
 describe("effect", () => {
   it('happy path', () => {
     const user = reactive({
@@ -14,5 +17,20 @@ describe("effect", () => {
     // update
     user.age++
     expect(nextAge).toBe(12)
+  })
+
+  it("should return runner when call effect", () => {
+    //1. effect(fn) -> function(runner) -> fn
+    let foo = 10
+    const runner = effect(() => {
+      foo++
+      return "foo"
+    })
+
+    expect(foo).toBe(11)
+    const r = runner()
+    expect(foo).toBe(12)
+    expect(r).toBe("foo")
+
   })
 }) 
